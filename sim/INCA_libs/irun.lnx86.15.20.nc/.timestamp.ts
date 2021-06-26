e79a7a77d4b8391de627fa129bbca268ffda5097
strong-ting <s8527502987@gmail.com>
2021-06-26 21:06:56 +0800

    -update .gitignore

--- sim/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
+++
@@ -1,5 +0,0 @@
-1600251382 /home/lin/github/tsmc13_neg.v
-1600329845 /home/lin/github/CIC_2014_grad/sim/testfixture2.v
-1600329831 /home/lin/github/CIC_2014_grad/sim/testfixture1.v
-1600327153 /home/lin/github/CIC_2014_grad/src/STI_DAC.v
-1600327198 /home/lin/github/CIC_2014_grad/syn/STI_DAC_syn.v
